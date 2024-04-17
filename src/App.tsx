f412b3b3eb68b8a7d1e839c4248ef3915f235b10
Henry Kaus <henrykaus19@gmail.com>
2024-04-16 19:46:57 -0700

    Initial state of application

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ReactElement, useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css';
+import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 
-function App() {
+interface Time {
+  hours: string | undefined;
+  minutes: string | undefined;
+}
+
+const emptyTime: Time = {
+  hours: undefined,
+  minutes: undefined,
+}
+
+const App = (): ReactElement => {
+  const [time, setTime] = useState<Time>(emptyTime);
+
+  const validateAndSetTime = (type: "hours" | "minutes", timeString: string) => {
+    const timeNumber = Number(timeString);
+    if (isNaN(timeNumber)) {
+      return;
+    }
+    switch (type) {
+      case "hours": {
+        if (!(timeNumber < 0) && !(timeNumber > 99)) {
+          setTime({
+            ...time,
+            hours: timeString.slice(0, 2),
+          })
+        }
+        break;
+      }
+      case "minutes": {
+        if (!(timeNumber < 0) && !(timeNumber > 59)) {
+          setTime({
+            ...time,
+            minutes: timeString.slice(0, 2),
+          })
+        }
+        break;
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <p className="App-header">
+        I worked
+        <input
+          value={time.hours}
+          placeholder="HH"
+          type="text"
+          name="hours"
+          className="App-time-input"
+          onChange={(event) =>
+            validateAndSetTime("hours", event.target.value)
+          }
+        />
+        :
+        <input
+          value={time.minutes}
+          placeholder="MM"
+          type="text"
+          name="minute"
+          className="App-time-input"
+          onChange={(event) =>
+            validateAndSetTime("minutes", event.target.value)
+          }
+        />
+      </p>
+      <button
+        className="App-go-button"
+        disabled={Number(time.minutes) <= 0 && Number(time.hours) <= 0}
+        onClick={() => console.log(time)}
+      >
+        <FontAwesomeIcon icon={faArrowRight}/>
+      </button>
+    </main>
   );
 }
 
